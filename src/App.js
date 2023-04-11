7372e390aa27c8aa64dbfedfeb67a14ac651dc0d
AndreyBallakh <andrey.ballakh@gmail.com>
2023-04-11 22:41:55 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
 import React from "react";
 import "./styles/App.css";
-import { BrowserRouter, Routes, Route, Switch, Navigate } from "react-router-dom";
-import About from "./pages/About";
-import Posts from "./pages/Posts";
+import { BrowserRouter } from "react-router-dom";
 import Navbar from "./components/UI/Navbar/Navbar";
+import AppRouter from "./components/AppRouter";
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
-      <Switch>
-        <Routes>
-          <Route path='/about' element={<About />} />
-          <Route path='/posts' element={<Posts />} />
-        </Routes>
-        <Navigate to='/posts' />
-      </Switch>
+      <AppRouter />
     </BrowserRouter>
   )
 }
